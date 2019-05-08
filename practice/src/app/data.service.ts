49722210eecb5fd1f2fbf3e5468d0eb4f4f94c67
Rick <ricardo@trebolbit.mx>
2019-05-08 11:19:54 -0500

    Added user details on click at the card

--- practice/src/app/data.service.ts
+++ practice/src/app/data.service.ts
@@ -11,5 +11,9 @@ export class DataService {
   getUsers() {
     return this.http.get('https://jsonplaceholder.typicode.com/users')
   }
-  
+
+  getUser(userId) {
+    return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+  }  
+
 }
