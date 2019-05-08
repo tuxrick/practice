265f9a58756d1035d9e62aefbf37476c66088b8b
Rick <ricardo@trebolbit.mx>
2019-05-08 09:40:58 -0500

    Added first project configuration , material design icons and first template SCSS theme

---
+++ practice/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    UsersComponent,
+    DetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
